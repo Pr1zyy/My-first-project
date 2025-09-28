893b5dbb5fd29b41f07de40cec7b0213f71e8860
Pr1zyy <ffhh28627@gmail.com>
2025-09-28 09:10:20 +0500

    Add files via upload
    release

---
+++ script.js
@@ -0,0 +1,21 @@
+function selectPeriod(evt, name) {
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(name).style.display = "flex";
+  evt.currentTarget.className += " active";
+  
+}
+
+function hideAlert() {
+  console.log(document.getElementsByClassName("alert")[0].style);
+  
+    document.getElementsByClassName("alert")[0].style.visibility="hidden"
+}
